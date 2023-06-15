cd2b65358ccf0d3f4c5b1d3d0535c4e82b9147be
Ajay <dev@ajay.app>
2023-06-15 00:35:54 -0400

    Add png logo for embeds

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
         description:
             "DeArrow is a browser extension for replacing titles and thumbnails on YouTube with community created accurate versions. No more clickbait.",
         author: "Ajay Ramachandran",
-        icon: "logo.svg",
+        icon: "logo-1024.png",
     },
     plugins: [
         "gatsby-plugin-react-helmet",
@@ -16,7 +16,7 @@ module.exports = {
                 start_url: "/",
                 background_color: "#fff",
                 theme_color: "#fff",
-                icon: "static/logo.svg",
+                icon: "static/logo-1024.png",
             },
         },
         "gatsby-plugin-sass",
