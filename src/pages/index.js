d76acbe49f3c24d18b1c01c0fb7797b8354a3c7b
Ajay <dev@ajay.app>
2023-08-06 02:43:19 -0400

    Hide beta notice when paywall enabled

--- src/pages/index.js
+++ src/pages/index.js
@@ -137,14 +137,17 @@ const IndexPage = () => {
                                 style={{ height: "58px" }} />
                         </a>
 
-                        <div className="paywall-notice">
-                            <div>
-                                Please note that DeArrow is in beta.{" "}
-                                DeArrow is free, forever, for anyone who installs during the beta. However,{" "}
-                                there will be a soft-paywall for new users in the future.{" "}
-                                <a href="https://gist.github.com/ajayyy/36a96ffc786f4e518fb62cac8b9674aa" rel="noreferrer" target="_blank">See details</a>
-                            </div>
-                        </div>
+                        {
+                            !paywallEnabled &&
+                                <div className="paywall-notice">
+                                    <div>
+                                        Please note that DeArrow is in beta.{" "}
+                                        DeArrow is free, forever, for anyone who installs during the beta. However,{" "}
+                                        there will be a soft-paywall for new users in the future.{" "}
+                                        <a href="https://gist.github.com/ajayyy/36a96ffc786f4e518fb62cac8b9674aa" rel="noreferrer" target="_blank">See details</a>
+                                    </div>
+                                </div>
+                        }
 
                         <h3>3rd Party Ports</h3>
 
