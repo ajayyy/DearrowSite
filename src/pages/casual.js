2a1baa48b342e949e7c1822a132dab3e5c86c82a
Ajay <dev@ajay.app>
2025-03-07 01:55:51 -0500

    Added a page for casual mode

---
+++ src/pages/casual.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+
+import { Link } from "gatsby";
+
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+import BeforeAfterImage from "../components/beforeAfterImage";
+import DownloadLinks from "../components/download-links";
+
+const CasualPage = () => {
+    return (
+        <Layout>
+            <Seo
+                title="DeArrow casual mode"
+            />
+
+            <div className="title">
+                <img src="/logo-casual.svg" alt="Logo" />
+
+                <span style={{ color: "white" }}>Casual mode</span>
+            </div>
+
+            <div className="container">
+                <div className="">
+                    <p>
+                        DeArrow is an open source browser extension for crowdsourcing better titles and thumbnails on YouTube.
+                        The goal is to make titles accurate and reduce sensationalism. No more arrows, ridiculous faces, and no more clickbait.
+                    </p>
+
+                    <p>
+                        With casual mode, you can keep original titles when they describe the video well enough and are clever or funny.
+                        Choose which categories of titles you want to see.
+                        You'll be able to vote if a title fits into a category or not.
+                    </p>
+
+                    <img src={"casual-vs-normal.png"} 
+                        className="before-after-image" 
+                        alt="Casual mode setting" />
+
+                    <iframe style={{ width: "100%" }} align="center" width="560" height="450" src="https://www.youtube-nocookie.com/embed/O3JGEDdB6Hc?modestbranding=1&rel=0" title="Demo Video"
+                        frameborder="0" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"
+                        allowfullscreen>
+                    </iframe>
+
+                    <DownloadLinks/>
+
+                    <p className="text-center">
+                        <a href="https://gist.github.com/ajayyy/9bfec83d57ea34f5182658ec8445aa9d">
+                            Privacy Policy (Human Readable)
+                        </a>
+                        {", "}
+                        <a href="https://gist.github.com/ajayyy/9e8100f069348e0bc062641f34d6af12">
+                            Terms of Use
+                        </a>
+                        {", "}
+                        <a href="donate">Donate</a>
+                    </p>
+                </div>
+            </div>
+        </Layout>
+    );
+};
+
+export default CasualPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,37 +4,10 @@ import { Link } from "gatsby";
 
 import Layout from "../components/layout";
 import Seo from "../components/seo";
-import { extensionLinks } from "../utils/links";
 import BeforeAfterImage from "../components/beforeAfterImage";
-
-const paywallEnabled = true;
+import DownloadLinks from "../components/download-links";
 
 const IndexPage = () => {
-    const [totalStats, setTotalStats] = useState({
-        activeUsers: "Loading",
-        titles: "Loading",
-        thumbnails: "Loading",
-    });
-
-    const [chromeLink, setChromeLink] = useState(extensionLinks.chrome);
-    const [firefoxLink, setFirefoxLink] = useState(extensionLinks.firefox);
-
-    useEffect(() => {
-        if (paywallEnabled) {
-            setChromeLink(`payment#link=chrome`);
-            setFirefoxLink(`payment#link=firefox`);
-        }
-    });
-
-    useEffect(() => {
-        fetch("https://sponsor.ajay.app/api/brandingStats")
-            .then((response) => response.json())
-            .then((resultData) => resultData.activeUsers !== undefined ? setTotalStats({
-                ...resultData,
-                activeUsers: Math.max(resultData.activeUsers, 27026 + 62896)
-            }) : null);
-    }, []);
-
     return (
         <Layout>
             <Seo
@@ -59,6 +32,22 @@ const IndexPage = () => {
                         image2="/after.jpg"
                     />
 
+                    <Link style={{display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            textDecoration: "none"
+                        }}
+                        to="/casual"
+                        target="_blank"
+                        rel="noreferrer">
+                        <img src="logo-casual.svg"
+                            style={{width: "35px", padding: "10px"}}/>
+
+                        <span>
+                            Too extreme? Also check out casual mode
+                        </span>
+                    </Link>
+
                     <p>
                         "Clickbait" isn't the exception anymore, it's becoming the norm.
                         Many have even started going through their entire backlog, changing old titles and thumbnails to be more attention grabbing and vague.
@@ -72,140 +61,11 @@ const IndexPage = () => {
                         DeArrow hopes to stop this cycle. It's time to return to a more peaceful experience.
                     </p>
 
-                    <div className="text-center">
-                        <h2>Download</h2>
-
-                        <a href={chromeLink}
-                        >
-                            <img
-                                src="/ChromeWebStore_BadgeWBorder_v2_206x58.png"
-                                alt="Download for Chrome"
-                            />
-                        </a>
-
-                        <a href={firefoxLink}
-                            style={{ paddingLeft: "15px" }}
-                        >
-                            <img
-                                src="/AMO-button_1.png"
-                                alt="Download for Firefox"
-                            />
-                        </a>
-
-                        <a
-                            href={"https://apps.apple.com/app/dearrow-for-youtube/id6451469297"}
-                            style={{ paddingLeft: "15px" }}
-                        >
-                            <img 
-                                src="/safari.svg" 
-                                alt="Download for Safari on macOS or iOS"
-                                style={{ height: "58px" }} />
-                        </a>
-
-                        <a
-                            href={chromeLink}
-                            style={{ paddingLeft: "15px" }}
-                        >
-                            <img
-                                src="/edge.png"
-                                alt="Download for Edge"
-                                style={{ height: "58px" }}
-                            />
-                        </a>
-
-                        <a
-                            href={chromeLink}
-                            style={{ paddingLeft: "15px" }}
-                        >
-                            <img src="/opera.png" alt="Download for Opera" />
-                        </a>
-
-                        <a
-                            href={"https://github.com/ajayyy/DeArrow/wiki/Android"}
-                            style={{ paddingLeft: "15px" }}
-                        >
-                            <img 
-                                src="/android.png" 
-                                alt="Download for Android"
-                                style={{ height: "58px" }} />
-                        </a>
-
-                        <a
-                            href={"https://apps.apple.com/app/dearrow-for-youtube/id6451469297"}
-                            style={{ paddingLeft: "15px" }}
-                        >
-                            <img 
-                                src="/IOS_logo.png" 
-                                alt="Download for iOS"
-                                style={{ height: "58px" }} />
-                        </a>
-
-                        <h3>3rd Party Ports</h3>
-
-                        <a
-                            href="https://wiki.sponsor.ajay.app/w/DeArrow/Community#Desktop"
-                            style={{ paddingLeft: "15px" }}
-                        >
-                            <img src="/computer.svg"
-                                alt="Download for Desktop"
-                                style={{ height: "30px" }} />
-                        </a>
-
-                        <a
-                            href="https://github.com/ajayyy/DeArrow/issues/39#issuecomment-1622742284"
-                            style={{ paddingLeft: "15px" }}
-                        >
-                            <img src="/invidious-logo.svg"
-                                alt="Userscript for Invidious"
-                                style={{ height: "30px" }} />
-                        </a>
-
-                        <a
-                            href="https://wiki.sponsor.ajay.app/w/DeArrow/Community#Discord"
-                            style={{ paddingLeft: "15px" }}
-                        >
-                            <img src="/discord.png"
-                                alt="Download for Discord Integrations"
-                                style={{ height: "30px" }} />
-                        </a>
-
-                        <a
-                            href="https://wiki.sponsor.ajay.app/w/DeArrow/Community#TV"
-                            style={{ paddingLeft: "15px" }}
-                        >
-                            <img src="/tv.png"
-                                alt="Download for TV"
-                                style={{ height: "30px" }} />
-                        </a>
-
-                        <a
-                            href="https://smarttubeapp.github.io/"
-                            style={{ paddingLeft: "15px" }}
-                        >
-                            <img src="/androidtv.png"
-                                alt="Download for Android TV"
-                                style={{ height: "30px" }} />
-                        </a>
-                    </div>
-
-                    <p>
-                        There are currently{" "}
-                        <strong>{totalStats.activeUsers.toLocaleString()}</strong>{" "}
-                        users who have submitted{" "}
-                        <strong>
-                            {totalStats.titles.toLocaleString()}
-                        </strong>{" "}
-                        titles and{" "}
-                        <strong>
-                            {totalStats.thumbnails.toLocaleString()}
-                        </strong>{" "}
-                        thumbnails.
-                    </p>
+                    <DownloadLinks/>
 
                     <p>
                         Watch a demo below!
                     </p>
-
 
                     <a 
                         href="https://www.youtube.com/watch?v=QjjpDhHh_QI"
